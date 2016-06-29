2549b38c1fc3acea8ea25de3037369c3ab5a87f1
kbyte <kbytesys@gmail.com>
2016-06-29 13:10:34 +0200

    simple test

--- snowpenguin/django/recaptcha2/tests.py
+++ snowpenguin/django/recaptcha2/tests.py
@@ -1,3 +1,23 @@
+import os
+
+from django.forms import Form
 from django.test import TestCase
 
-# Create your tests here.
+from snowpenguin.django.recaptcha2.fields import ReCaptchaField
+from snowpenguin.django.recaptcha2.widgets import ReCaptchaWidget
+
+
+class RecaptchaTestForm(Form):
+    recaptcha = ReCaptchaField(widget=ReCaptchaWidget())
+
+
+class TestRecaptchaForm(TestCase):
+    def setUp(self):
+        os.environ['RECAPTCHA_DISABLE'] = 'True'
+
+    def test_dummy_validation(self):
+        form = RecaptchaTestForm({})
+        self.assertTrue(form.is_valid())
+
+    def tearDown(self):
+        del os.environ['RECAPTCHA_TESTING']
