7a28f5e75fad90892644b96a02f3691c3f678437
Andrea Briganti <kbytesys@gmail.com>
2018-08-20 19:01:59 +0200

    initial (but ugly) recaptcha invisible support

--- snowpenguin/django/recaptcha2/widgets.py
+++ snowpenguin/django/recaptcha2/widgets.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.forms.widgets import Widget
 from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
+from django.utils.translation import gettext as _
 
 
 class ReCaptchaWidget(Widget):
@@ -41,6 +42,35 @@ class ReCaptchaWidget(Widget):
                 'tabindex': self.tabindex,
                 'callback': self.callback,
                 'expired_callback': self.expired_callback
+            })
+        )
+
+    def value_from_datadict(self, data, files, name):
+        return [data.get('g-recaptcha-response', None)]
+
+
+class ReCaptchaInvisibleWidget(Widget):
+    def __init__(self, public_key=None, submit_label=_('Submit'), extra_css_classes=None, form_id=None, attrs={},
+                 *args, **kwargs):
+        super(ReCaptchaInvisibleWidget, self).__init__(*args, **kwargs)
+        self.attrs = attrs
+        self._public_key = public_key
+        self.submit_label = submit_label
+        self.extra_css_classes = extra_css_classes
+        self.form_id = form_id
+
+    def render(self, name, value, attrs=None, *args, **kwargs):
+        template = 'snowpenguin/recaptcha/recaptcha_invisible_automatic.html'
+
+        generated_id = '%s' % randint(10000, 99999)
+
+        return mark_safe(
+            render_to_string(template, {
+                'generated_id': generated_id,
+                'public_key': self._public_key or settings.RECAPTCHA_PUBLIC_KEY,
+                'form_id': self.form_id,
+                'submit_label': self.submit_label,
+                'extra_css_classes': self.extra_css_classes
             })
         )
 
