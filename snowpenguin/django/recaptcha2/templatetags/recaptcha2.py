369a392b8c3ff22c5b77852d3b4f250ac54af495
Andrea Briganti <kbytesys@gmail.com>
2018-08-20 19:49:49 +0200

    invisible recaptcha as a template tag instead of a widget.

--- snowpenguin/django/recaptcha2/templatetags/recaptcha2.py
+++ snowpenguin/django/recaptcha2/templatetags/recaptcha2.py
@@ -1,5 +1,8 @@
+from random import randint
+
 from django import template
 from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
 register = template.Library()
 
@@ -22,3 +25,18 @@ def recaptcha_explicit_init(language=None):
 @register.inclusion_tag('snowpenguin/recaptcha/recaptcha_explicit_support.html')
 def recaptcha_explicit_support():
     return {}
+
+
+@register.inclusion_tag('snowpenguin/recaptcha/recaptcha_invisible_button.html')
+def recaptcha_invisible_button(public_key=None, submit_label=None, extra_css_classes=None, form_id=None,
+                               custom_callback=None):
+    generated_id = '%s' % randint(10000, 99999)
+
+    return {
+        'generated_id': generated_id,
+        'public_key': public_key or settings.RECAPTCHA_PUBLIC_KEY,
+        'form_id': form_id,
+        'submit_label': submit_label or _('Submit'),
+        'extra_css_classes': extra_css_classes,
+        'custom_callback': custom_callback
+    }
--- snowpenguin/django/recaptcha2/widgets.py
+++ snowpenguin/django/recaptcha2/widgets.py
@@ -1,10 +1,9 @@
 from random import randint
 
 from django.conf import settings
-from django.forms.widgets import Widget
+from django.forms.widgets import Widget, Input
 from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
-from django.utils.translation import gettext as _
 
 
 class ReCaptchaWidget(Widget):
@@ -49,30 +48,11 @@ class ReCaptchaWidget(Widget):
         return [data.get('g-recaptcha-response', None)]
 
 
-class ReCaptchaInvisibleWidget(Widget):
-    def __init__(self, public_key=None, submit_label=_('Submit'), extra_css_classes=None, form_id=None, attrs={},
-                 *args, **kwargs):
-        super(ReCaptchaInvisibleWidget, self).__init__(*args, **kwargs)
-        self.attrs = attrs
-        self._public_key = public_key
-        self.submit_label = submit_label
-        self.extra_css_classes = extra_css_classes
-        self.form_id = form_id
+class ReCaptchaInvisibleWidget(Input):
+    input_type = 'hidden'
 
-    def render(self, name, value, attrs=None, *args, **kwargs):
-        template = 'snowpenguin/recaptcha/recaptcha_invisible_automatic.html'
-
-        generated_id = '%s' % randint(10000, 99999)
-
-        return mark_safe(
-            render_to_string(template, {
-                'generated_id': generated_id,
-                'public_key': self._public_key or settings.RECAPTCHA_PUBLIC_KEY,
-                'form_id': self.form_id,
-                'submit_label': self.submit_label,
-                'extra_css_classes': self.extra_css_classes
-            })
-        )
+    def render(self, name, value, attrs=None, renderer=None):
+        return ''
 
     def value_from_datadict(self, data, files, name):
         return [data.get('g-recaptcha-response', None)]
